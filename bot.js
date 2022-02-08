267d753e30e995e19fbf051b3345caeef707804d
Chad Stewart <chadrhonanstewart@gmail.com>
2022-02-08 09:50:16 -0500

    Added quick and dirty ban list to bot.

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
-// Our Twitter library
 const Twit = require("twit");
-
-// We need to include our configuration file...
+const banned = require("./banned/banned.json");
 const twit = new Twit(require("./config/config.js"));
 
-const stream = twit.stream('statuses/filter', {track: '#TechIsHiring'});
+const stream = twit.stream("statuses/filter", { track: "#TechIsHiring" });
 
-function responseCallback (err, data, response) {
+function responseCallback(err, data, response) {
   console.log(err);
- }
+}
 
- stream.on('tweet', tweet => {
-  // retweet
- twit.post('statuses/retweet/:id', {id: tweet.id_str}, responseCallback);
- // like
- twit.post('favorites/create', {id: tweet.id_str}, responseCallback);
+stream.on("tweet", (tweet) => {
+  const isNotBanned = !banned.includes(tweet.user.id);
+
+  if (isNotBanned) {
+    // retweet
+    twit.post("statuses/retweet/:id", { id: tweet.id_str }, responseCallback);
+    // like
+    twit.post("favorites/create", { id: tweet.id_str }, responseCallback);
+  }
 });
